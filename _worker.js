ab19583ba29d13a9ced6132cffcec0a6c3b8badd
gshtwy <161450968+gshtwy@users.noreply.github.com>
2024-09-21 23:13:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,6 +5,7 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
+	'cf.090227.xyz:443#加入我的频道t.me/cncomorg解锁更多优选节点',
 	'icook.tw:2053#官方优选域名',
 	'cloudflare.cfgo.cc#优选官方线路',
 ];
